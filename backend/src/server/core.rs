2f4599984a7a79ffe83f06340a7ea10ce263ec4f
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2024-12-25 13:26:00 -0500

    Tuned logging levels for the backend components
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

--- backend/src/server/core.rs
+++ backend/src/server/core.rs
@@ -81,9 +81,9 @@ pub async fn run_server(host: String, port: u16) -> Result<(), Box<dyn std::erro
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
                 // axum logs rejections from built-in extractors with the `axum::rejection`
-                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events.
                 format!(
-                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    "{}=debug,tower_http=info,axum::rejection=trace",
                     env!("CARGO_CRATE_NAME")
                 )
                 .into()
