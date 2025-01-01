87836f70fb55ded90235f5d7afd1441e941d2152
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2024-12-31 22:49:53 -0500

    Updated backend version and frontend config
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

--- kamekai/src/config/auth.tsx
+++ kamekai/src/config/auth.tsx
@@ -1,6 +1,6 @@
 export const cognitoConfig = {
-  authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_G0l5wgf1W',
-  client_id: '1ti7du87tqnv03glf0odrtj4dj',
+  authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_RIfjoPiFg',
+  client_id: '79etm593hkt3a1f9jftuptubap',
   redirect_uri: 'http://localhost:1420/auth/callback',
   logout_uri: 'http://localhost:1420/auth/logout',
   domain: 'auth.seafoodfry.ninja',
