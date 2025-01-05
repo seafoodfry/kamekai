dbb701c4cf440a000b35512762d84020b55251c8
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2025-01-04 22:54:36 -0600

    New user pool
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

--- kamekai/src/config/auth.tsx
+++ kamekai/src/config/auth.tsx
@@ -1,6 +1,6 @@
 export const cognitoConfig = {
-  authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_NipWF0bjs',
-  client_id: '4pq157afdl52qbhsfmtbj0536k',
+  authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ES1AIyLI9',
+  client_id: '6351vlk6qmcro8jde3onl3ug8h',
   redirect_uri: 'tauri://localhost', //'tauri://com.kamekai.app/auth/callback',
   logout_uri: 'tauri://localhost', //'tauri://com.kamekai.app/auth/logout',
   domain: 'auth.seafoodfry.ninja',
