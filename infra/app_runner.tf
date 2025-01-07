fd3d305916bb90cf33b0c1185a57790b95cb8e99
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2025-01-06 21:02:15 -0600

    App runner update: WIP instrumented handler
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

--- infra/app_runner.tf
+++ infra/app_runner.tf
@@ -8,7 +8,7 @@ resource "aws_apprunner_service" "kamekai" {
 
     image_repository {
       image_repository_type = "ECR"
-      image_identifier      = "${aws_ecr_repository.kamekai.repository_url}:1.0.2-b8d8fa60"
+      image_identifier      = "${aws_ecr_repository.kamekai.repository_url}:2.0.0-763e1cee"
 
       image_configuration {
         port = "8080"
